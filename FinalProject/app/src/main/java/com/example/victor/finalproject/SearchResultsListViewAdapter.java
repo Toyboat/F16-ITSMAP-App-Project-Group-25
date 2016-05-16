593a569244ea3caf7112aa51b3874cda6ccffb04
jtor369 <jtor369@gmail.com>
2016-05-16 11:26:48 +0200

    1126-160516

---
+++ FinalProject/app/src/main/java/com/example/victor/finalproject/SearchResultsListViewAdapter.java
@@ -0,0 +1,32 @@
+package com.example.victor.finalproject;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Jacob on 16/05/2016.
+ */
+public class SearchResultsListViewAdapter extends BaseAdapter{
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
