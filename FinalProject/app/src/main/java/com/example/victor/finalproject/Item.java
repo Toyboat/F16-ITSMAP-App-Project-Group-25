68a890c6f5b8a6438335b8bd91688d390ebe8094
jtor369 <jtor369@gmail.com>
2016-05-16 11:04:54 +0200

    Item Class

---
+++ FinalProject/app/src/main/java/com/example/victor/finalproject/Item.java
@@ -0,0 +1,35 @@
+package com.example.victor.finalproject;
+
+import android.graphics.Bitmap;
+import android.location.Location;
+
+import java.util.List;
+
+/**
+ * Created by Jacob on 16/05/2016.
+ */
+public class Item {
+    public final int id;
+    public final String description;
+
+    public final Location location;
+    public final int userid;
+    public final int timestamp;
+
+    private Bitmap thumbnail;
+    private List<Bitmap> photos;
+
+    public final List<String> tags;
+
+    public Item(int id, String description, Location location, int userid, int timestamp, List<String> tags)
+    {
+        this.id = id;
+        this.description = description;
+        this.location = location;
+        this.userid = userid;
+        this.timestamp = timestamp;
+        this.tags = tags;
+    }
+
+
+}
