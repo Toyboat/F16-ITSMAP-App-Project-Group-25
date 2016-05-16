65782af464d97b7ef6656652b04649999af00f79
Victor Staffe <victor.staffe@hotmail.com>
2016-05-16 12:50:01 +0200

    Lost/Found/main activity connected

--- FinalProject/app/src/main/java/com/example/victor/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/victor/finalproject/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.victor.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button lostButton = (Button) findViewById(R.id.foundButton);
+    private Button foundButton = (Button) findViewById(R.id.lostButton);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    lostButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(MainActivity.this, LostActivity.class);
+            startActivity(i);
+        }
+    });
+    foundButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(MainActivity.this, FoundActivity.class);
+            startActivity(i);
+        }
+    });
     }
+
 }
