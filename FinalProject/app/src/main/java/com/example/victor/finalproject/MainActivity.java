0d60c97aadf98b47decb7f7e772b5605d02474e1
Victor Staffe <victor.staffe@hotmail.com>
2016-05-17 11:25:48 +0200

    searchButton in mainActivity, and some more fragment design

--- FinalProject/app/src/main/java/com/example/victor/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/victor/finalproject/MainActivity.java
@@ -9,12 +9,14 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     private Button lostButton;
     private Button foundButton;
+    private Button searchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lostButton  = (Button) findViewById(R.id.lostButton);
         foundButton = (Button) findViewById(R.id.foundButton);
+        searchButton = (Button) findViewById(R.id.searchButton);
         lostButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SearchResultsActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
 }
