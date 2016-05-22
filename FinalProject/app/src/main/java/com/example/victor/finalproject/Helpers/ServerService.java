4e8c42266ed0b0113815b5dc7516619d143f5a87
jtor369 <jtor369@gmail.com>
2016-05-22 19:30:49 +0200

    Volley added; Test button REQUEST, fetches items from server

--- FinalProject/app/src/main/java/com/example/victor/finalproject/Helpers/ServerService.java
+++ FinalProject/app/src/main/java/com/example/victor/finalproject/Helpers/ServerService.java
@@ -1,12 +1,25 @@
 package com.example.victor.finalproject.Helpers;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.victor.finalproject.Datacontainers.Item;
+import com.example.victor.finalproject.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,12 +41,49 @@ public class ServerService extends Service {
         return binder;
     }
 
-    public static void searchFor(Item i)
+    //public static void searchFor(Item i)
+    public static void searchFor(Context context,Item i)
     {
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        String address = context.getResources().getString(R.string.server_address);
+        String scriptname = context.getResources().getString(R.string.server_search_script);
+        String url = address + scriptname + "?jsonItem=";
 
+        url += i.toJSONString();
+        Log.println(Log.DEBUG,"StringRequest","URL: "+url);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.println(Log.DEBUG,"StringRequest","GOT: "+response);
+                try {
+                    JSONArray arr = new JSONArray(response);
+                    List<Item> items = new ArrayList<Item>();
+                    for (int x = 0; x < arr.length(); x++)
+                    {
+                        items.add(Item.fromJSONString(arr.get(x).toString()));
+                    }
+                    //Item item = Item.fromJSONString(response);
+                    ServerService.items = items;
+                }catch(Exception e)
+                {
+                    e.printStackTrace();
+                    Log.println(Log.DEBUG,"StringRequest","Error: " +  response);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.println(Log.DEBUG,"StringRequest","Error!");
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(stringRequest);
 
     }
 
+
+    public static List<Item> items;
     public static List<Item> getResults(){
         //TODO: getDataFromServer
 /*
@@ -50,12 +100,14 @@ public class ServerService extends Service {
 
         //=====================================end of dummy list
         */
-        List dummyList = new ArrayList<Item>();
-        for (int i= 0; i < 20; i++)
-        {
-            dummyList.add(Item.getDummy());
+        if (items == null) {
+            List dummyList = new ArrayList<Item>();
+            for (int i = 0; i < 20; i++) {
+                dummyList.add(Item.getDummy());
+            }
+            items = dummyList;
         }
-        return dummyList;
+        return items;
     }
 
     private int storeItem(Item i){
--- FinalProject/app/src/main/java/com/example/victor/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/victor/finalproject/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.victor.finalproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.victor.finalproject.Datacontainers.Item;
+import com.example.victor.finalproject.Helpers.ServerService;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private Button lostButton;
     private Button foundButton;
     private Button searchButton;
+    private Button requestButton;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
         lostButton  = (Button) findViewById(R.id.lostButton);
         foundButton = (Button) findViewById(R.id.foundButton);
         searchButton = (Button) findViewById(R.id.searchButton);
+
+        requestButton = (Button) findViewById(R.id.btnRequest);
+
         lostButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,6 +51,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        requestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ServerService a = new ServerService();
+                //public Item(int id, String description, Location location, int userid, int timestamp, List<String> tags, Bitmap thumbnail)
+
+                ServerService.searchFor(context,new Item(10,"",Item.JSONLocationParse("{\"lat\":56.0, \"lon\":10.0,\"radius\":1000}"),10,10,new ArrayList<String>(), null));
+            }
+        });
     }
 
 }
