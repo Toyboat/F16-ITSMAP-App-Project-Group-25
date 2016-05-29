47fdc53b1fd9d94b48bc4a539fe6e75ff26db9bd
Victor Staffe <victor.staffe@hotmail.com>
2016-05-29 13:09:55 +0200

    sidste ændringer til maps

--- FinalProject/app/src/main/java/com/example/victor/finalproject/Fragments/WhereFragment.java
+++ FinalProject/app/src/main/java/com/example/victor/finalproject/Fragments/WhereFragment.java
@@ -1,12 +1,17 @@
 package com.example.victor.finalproject.Fragments;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
@@ -38,12 +43,14 @@ public class WhereFragment extends Fragment {
     private GoogleMap map;
     private double userLatitude;
     private double userLongitude;
+    private double latitude, longitude;
     public static final String USER_LATITUDE = "location_latitude";
     public static final String USER_LONGITUDE = "location_longitude";
     private boolean userLocationKnown = false;
     private boolean tracing = true;
     private int mapType = GoogleMap.MAP_TYPE_NORMAL;
-
+    private Location userLocation = null;
+    private LocationManager locationManager;
 
     public WhereFragment() {
         // Required empty public constructor
@@ -60,17 +67,18 @@ public class WhereFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         Intent data = getActivity().getIntent();
-        if(data.hasExtra(USER_LONGITUDE) && data.hasExtra(USER_LATITUDE)){
+        if (data.hasExtra(USER_LONGITUDE) && data.hasExtra(USER_LATITUDE)) {
 
             userLatitude = data.getDoubleExtra(USER_LATITUDE, 0);
             userLongitude = data.getDoubleExtra(USER_LONGITUDE, 0);
-            if(userLatitude!=0 && userLongitude!=0) {
+            if (userLatitude != 0 && userLongitude != 0) {
                 userLocationKnown = true;
             }
         }
         //zoomToUser();//gets location?
 //        setUpMapIfNeeded();//what do?
-        Log.d("maps","longandlat" + getLocation().toString());
+        locationUpdateBroadcast(userLocation);
+        Log.d("maps", "longandlat" + getLocation().toString());
     }
 
     @Override
@@ -79,6 +87,7 @@ public class WhereFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_where, container, false);
     }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -107,10 +116,10 @@ public class WhereFragment extends Fragment {
     public void expand(String s) {
         LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (s == "lost") {
-            Log.d("pre.maps","longandlat" + getLocation().toString());
+            Log.d("pre.maps", "longandlat" + getLocation().toString());
             view = inflater.inflate(R.layout.where_lost_opened, null);
             //Location returnLocation = getLocation();
-            Log.d("mapsexpander","longandlat" + getLocation().toString());
+            Log.d("mapsexpander", "longandlat" + getLocation().toString());
         } else {
             view = inflater.inflate(R.layout.where_found_opened, null);
         }
@@ -147,34 +156,45 @@ public class WhereFragment extends Fragment {
     public Location getUserLocation() {
         return null;
     }
+
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (map == null) {
             // Try to obtain the map from the SupportMapFragment.
-         //   map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            //   map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
             // Check if we were successful in obtaining the map.
             if (map != null) {
                 setUpMap();
             }
         }
     }
-    private void setUpMap() {
-        if(userLocationKnown) {
 
-            if(!tracing){
+    private void setUpMap() {
+        if (userLocationKnown) {
+
+            if (!tracing) {
                 map.clear();   //if we are not tracking, remove the marker first
             }
             map.addMarker(new MarkerOptions().position(new LatLng(userLatitude, userLongitude)).title("You are here!"));
         }
     }
 
-    private void zoomToUser(){
-        if(userLocationKnown) {
+    private void zoomToUser() {
+        if (userLocationKnown) {
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(
                     new LatLng(userLatitude, userLongitude), 12));
         } else {
-            Log.d("Error","Unknown user location");
+            Log.d("Error", "Unknown user location");
         }
+    }
+
+    private void locationUpdateBroadcast(Location location) {
+
+        Intent update = new Intent("LOCATION_UPDATE");
+        update.putExtra(USER_LATITUDE, location.getLatitude());
+        update.putExtra(USER_LONGITUDE, location.getLongitude());
+        LocalBroadcastManager.getInstance(getContext()).sendBroadcast(update);
+
     }
 
     BroadcastReceiver locationUpdateReceiver = new BroadcastReceiver() {
@@ -183,11 +203,11 @@ public class WhereFragment extends Fragment {
 
             //Toast.makeText(getApplicationContext(), "Got location update", Toast.LENGTH_SHORT).show();
 
-            if(data.hasExtra(USER_LONGITUDE) && data.hasExtra(USER_LATITUDE)){
+            if (data.hasExtra(USER_LONGITUDE) && data.hasExtra(USER_LATITUDE)) {
 
                 userLatitude = data.getDoubleExtra(USER_LATITUDE, 0);
                 userLongitude = data.getDoubleExtra(USER_LONGITUDE, 0);
-                if(userLatitude!=0 && userLongitude!=0) {
+                if (userLatitude != 0 && userLongitude != 0) {
                     userLocationKnown = true;
                 }
             }
@@ -195,4 +215,71 @@ public class WhereFragment extends Fragment {
         }
     };
 
+    private void updateStatus() {
+        String status = "";
+        if (userLocation != null) {
+            status += " location=known";
+            latitude = userLocation.getLatitude();
+            longitude = userLocation.getLongitude();
+            //txtPosition.setText("Lat: \t" + latitude + "\nLong: \t" + longitude);
+        } else {
+            //txtPosition.setText("Unknown\n");
+            status += " location=unknown";
+
+        }
+
+        if (tracing) {
+            status += " tracking=true";
+        } else {
+            status += " tracking=false";
+        }
+
+        if (userLocation == null && locationManager != null) {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            Location lastGps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location lastNetwork = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            status += " \nlast_known_location=";
+            if(lastGps!=null){
+                status += "{" + lastGps.getLatitude() + ";" + lastGps.getLongitude() + "}(GPS) ";
+            }
+            if(lastNetwork!=null){
+                status += "{" + lastNetwork.getLatitude() + ";" + lastNetwork.getLongitude() + "}(Network) ";
+            }
+        }
+
+        Log.d("updatate status:","Status:" + status);
+    }
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            userLocation = location;
+            updateStatus();
+            locationUpdateBroadcast(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 }
